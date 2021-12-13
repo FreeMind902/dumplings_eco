1769e0a63d6dd345a11556b9c730d7f0b35ff946
FreeMind902 <h31n3r@eb.de>
2021-12-13 22:54:22 +0100

    working full, testing

--- resources/views/elements/frontend/footer.blade.php
+++ resources/views/elements/frontend/footer.blade.php
@@ -5,38 +5,38 @@
 
       <div class="col-12 col-md-6 mt-3 d-none d-md-block">
 
-        <span class=""><b>Wannaeat</b></span>
+        <span class=""><b>Wanna Eat</b></span>
         <hr class="footer-hr">
-        <span>
+        <span class="pe-4">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
           </span>
 
       </div>
 
-      <div class="col-12 col-md-3 ps-5 mt-3">
+      <div class="col-12 col-md-3 mt-3">
         <span class=""><b>Links</b></span>
         <hr class="footer-hr">
         <ul class="nav flex-column">
           <li class="nav-item">
-            <a class="nav-link text-decoration-none text-dark" href="{{ route('frontend.index') }}">Story</a>
+            <a class="nav-link text-decoration-none text-dark" href="{{ route('frontend.index') }}" style="margin-left: -16px">Story</a>
           </li>
           <li class="nav-item ">
-            <a class="nav-link text-decoration-none text-dark" href="{{ route('frontend.foodlist') }}">Our Foods</a>
+            <a class="nav-link text-decoration-none text-dark" href="{{ route('frontend.foodlist') }}" style="margin-left: -16px">Our Foods</a>
           </li>
           <li class="nav-item ">
-            <a class="nav-link text-decoration-none text-dark" href="{{ route('frontend.approach') }}">Anfahrt</a>
+            <a class="nav-link text-decoration-none text-dark" href="{{ route('frontend.approach') }}" style="margin-left: -16px">Anfahrt</a>
           </li>
           <li class="nav-item ">
-            <a class="nav-link text-decoration-none text-dark" href="{{ route('frontend.impress') }}">Impressum</a>
+            <a class="nav-link text-decoration-none text-dark" href="{{ route('frontend.impress') }}" style="margin-left: -16px">Impressum</a>
           </li>
           <li class="nav-item ">
-            <a class="nav-link text-decoration-none text-dark" href="{{ route('frontend.privacy') }}">Datenschutz</a>
+            <a class="nav-link text-decoration-none text-dark" href="{{ route('frontend.privacy') }}" style="margin-left: -16px">Datenschutz</a>
           </li>
         </ul>
 
       </div>
 
-      <div class="col-12 col-md-3 ps-5 mt-3">
+      <div class="col-12 col-md-3 mt-3">
 
         <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
           <span><b>Kontakt</b></span>
@@ -49,7 +49,7 @@
           <span>
            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                 stroke-linejoin="round" class="feather feather-home"><path
-               d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>
+                 d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>
           </span>
           <span>Flughafenstr. 25, 12053 Berlin </span>
         </div>
@@ -68,10 +68,10 @@
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                  stroke-linejoin="round" class="feather feather-phone">
               <path
-                d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"/>
+                  d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"/>
             </svg>
           </span>
-{{--          <span>+49123646789</span>--}}
+          {{--          <span>+49123646789</span>--}}
         </div>
         <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 mt-2">
             <span>
@@ -80,7 +80,7 @@
               <rect x="5" y="2" width="14" height="20" rx="2" ry="2"/><line x1="12" y1="18" x2="12.01" y2="18"/>
               </svg>
             </span>
-{{--          <span>+49123646789</span>--}}
+          {{--          <span>+49123646789</span>--}}
         </div>
 
       </div>
